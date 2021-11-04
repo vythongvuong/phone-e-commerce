0ad80747e244ba76f82bfe9bfb891471578e7a07
vythongv <vythongv@gmail.com>
2021-11-05 02:07:25 +0700

    add dto

---
+++ backend/Controllers/TodoController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+        
+        
+        // GET
+        // [HttpGet("/index")]
+        // public async Task<ActionResult<IEnumerable<TodoContext>>> Index()
+        // {
+        //     var res = await _context.TodoItems.FindAsync();
+        //     return res;
+        // }
+    }
+}
---
+++ backend/dto/TodoItemDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace backend.dto
+{
+    public class TodoItemDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
